27fcc6e054f309e616beeef3a98f47ef49be351c
kavita <kavitak@digitsoftsol.com>
2024-08-10 15:03:56 +0530

    coading standard

---
+++ app/code/Dss/OrderDetails/Block/Tax.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+/**
+* Digit Software Solutions..
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the EULA
+* that is bundled with this package in the file LICENSE.txt.
+*
+* @category   Dss
+* @package    Dss_OrderDetails
+* @author     Extension Team
+* @copyright Copyright (c) 2024 Digit Software Solutions. ( https://digitsoftsol.com )
+*/
+namespace Dss\OrderDetails\Block;
+
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Tax\Helper\Data;
+use Magento\Tax\Model\Config;
+
+class Tax extends \Magento\Tax\Block\Sales\Order\Tax
+{
+    /**
+     * Tax Constructor
+     * @param Context $context
+     * @param Config $taxConfig
+     * @param Data $helper
+     * @param array $data
+     */
+    public function __construct(
+        protected Context $context,
+        protected Config $taxConfig,
+        protected Data $helper,
+        array $data = []
+    ) {
+        parent::__construct($context, $taxConfig, $data);
+    }
+
+    /**
+     * Get Helper Tax
+     *
+     * @return \Magento\Tax\Helper\Data
+     */
+    public function getHelper()
+    {
+        return $this->helper;
+    }
+}
---
+++ app/code/Dss/OrderDetails/registration.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Dss Co.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://Dss.com/Dss-License.txt
+ *
+ * =================================================================
+ *
+ * MAGENTO EDITION USAGE NOTICE
+ * =================================================================
+ * This package designed for Magento COMMUNITY edition
+ * Dss does not guarantee correct work of this extension
+ * on any other Magento edition except Magento COMMUNITY edition.
+ * Dss does not provide extension support in case of
+ * incorrect edition usage.
+ * =================================================================
+ *
+ * @category   Dss
+ * @package    Dss_OrderDetails
+ * @author     Extension Team
+ * @copyright  Copyright (c) 2015-2016 Dss Co. ( http://Dss.com )
+ * @license    http://Dss.com/Dss-License.txt
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Dss_OrderDetails',
+    __DIR__
+);
