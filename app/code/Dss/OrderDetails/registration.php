74fe248589f2d6668ff8a29269fd23a514234e8c
kavita <kavitak@digitsoftsol.com>
2024-08-13 14:33:14 +0530

    coding standards fixed 13-08-2024

--- app/code/Dss/OrderDetails/registration.php
+++ app/code/Dss/OrderDetails/registration.php
@@ -1,30 +1,19 @@
 <?php
 /**
- * Dss Co.
+ * Magexperts Co.
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the EULA
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://Dss.com/Dss-License.txt
+ * http://magexperts.com/Magexperts-License.txt
  *
- * =================================================================
- *
- * MAGENTO EDITION USAGE NOTICE
- * =================================================================
- * This package designed for Magento COMMUNITY edition
- * Dss does not guarantee correct work of this extension
- * on any other Magento edition except Magento COMMUNITY edition.
- * Dss does not provide extension support in case of
- * incorrect edition usage.
- * =================================================================
- *
- * @category   Dss
- * @package    Dss_OrderDetails
+ * @category   Magexperts
+ * @package    Magexperts_CookieNotice
  * @author     Extension Team
- * @copyright  Copyright (c) 2015-2016 Dss Co. ( http://Dss.com )
- * @license    http://Dss.com/Dss-License.txt
+ * @copyright  Copyright (c) 2015-2016 Magexperts Co. ( http://magexperts.com )
+ * @license    http://magexperts.com/Magexperts-License.txt
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
